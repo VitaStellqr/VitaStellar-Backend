297d44fcfd4f5c5efebbc70c0ef99f6344148211
abojobo1 <a-ojobo@leadway-group.com>
2026-03-29 22:24:33 +0100

    Add Swagger docs for notification endpoints and checks for unread workflows

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationPreferencesModule } from './preferences/preferences.module';
 import { NotificationPreference } from './entities/notification-preference.entity';
+import { Notification } from './entities/notification.entity';
 import { NotificationService } from './services/notification.service';
+import { NotificationsController } from './notifications.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([NotificationPreference]),
+    TypeOrmModule.forFeature([NotificationPreference, Notification]),
     NotificationPreferencesModule,
   ],
+  controllers: [NotificationsController],
   providers: [NotificationService],
   exports: [NotificationPreferencesModule, NotificationService],
 })
