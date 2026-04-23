76903ea1e8dd7d6a3cff833a9d195e51a97bf508
ummarig <nafiuigee@gmail.com>
2026-04-23 12:48:23 +0000

    implemented the task completion

--- src/modules/health-tasks/health-tasks.module.ts
+++ src/modules/health-tasks/health-tasks.module.ts
@@ -3,13 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthTasksController } from './health-tasks.controller';
 import { HealthTasksService } from './health-tasks.service';
 import { HealthTask } from '../../tasks/entities/health-task.entity';
+import { TaskCompletion } from '../../database/entities/task-completion.entity';
+import { User } from '../../entities/user.entity';
 import { PriorityService } from './services/priority.service';
 import { ArchiveService } from './services/archive.service';
+import { CompletionService } from './services/completion.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HealthTask])],
+  imports: [TypeOrmModule.forFeature([HealthTask, TaskCompletion, User])],
   controllers: [HealthTasksController],
-  providers: [HealthTasksService, PriorityService, ArchiveService],
-  exports: [HealthTasksService],
+  providers: [HealthTasksService, PriorityService, ArchiveService, CompletionService],
+  exports: [HealthTasksService, CompletionService],
 })
 export class HealthTasksModule {}
